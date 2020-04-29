2e717e1c3af688a7ee74b5ba513176095042aa35
Vlad Ageev <vageev06@gmail.com>
2020-04-29 10:36:30 +0200

    Added functionality for mobile header

---
+++ static/js/app.js
@@ -0,0 +1,44 @@
+class App {
+    constructor() {
+        this.isNavOpen = false;
+    }
+    mobileHeader() {
+        $('#mobileCatalog').click(() => {
+            $('#catalog').css('display' , 'flex');
+            $('#greyBackgound').css('display' , 'block');
+        });
+
+        $('#menuButton').click(() => {
+            $('#mobileMenu1').css('display' , 'flex');
+            $('#greyBackgound').css('display' , 'block');
+        });
+
+        $('#searchIcon').click(() => {
+            $('#searchField').css('display' , 'flex');
+            $('#greyBackgound').css('display' , 'block');
+        }); 
+    }
+    header(){
+        
+    }
+    closeButton(element , ...elements){
+        // element is the element that we wonna close
+        // elements are several extra elements that we wonna close if they're required
+        $(`#${element}`).css('display' , 'none');
+        if(elements.length !== 0){
+            elements.forEach(element => {
+                $(`#${element}`).css('display' , 'none');
+            });
+        }
+    }
+}
+
+const app = new App();
+
+if($('body')[0].clientWidth < 768){
+    app.mobileHeader();
+}
+else{
+    app.header();
+}
+    
